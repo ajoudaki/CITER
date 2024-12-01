ffa58f0eb2d4b1397b2401ff3df500e5ce35348b
ajoudaki <ajoudaki@inf.ethz.ch>
2024-12-01 21:43:11 +0100

    refactor

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import numpy as np
 
 from config import TrainingConfig
-from train_validate import TrainingManager
+from trainer import TrainingManager
 
 config = TrainingConfig(
     project_name="citation-matching",
