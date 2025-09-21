bb092f208ddbe9885650d18434518f788f84948b
ajoudaki <ajoudaki@inf.ethz.ch>
2025-09-21 19:35:41 +0200

    adding preproc script

---
+++ preprocess.sh
@@ -0,0 +1,3 @@
+head -n 1000 data/lemmas_theorems.jsonl > data/lemmas_theorems_tiny.jsonl
+head -n 10000 data/lemmas_theorems.jsonl > data/lemmas_theorems_small.jsonl
+head -n 100000 data/lemmas_theorems.jsonl > data/lemmas_theorems_medium.jsonl
